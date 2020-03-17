778b73cc8c3b8ef21367de0dde4eebd4505f2aa3
TimJ <timj@elastacloud.com>
2020-03-17 19:06:52 +0000

    First implementation of a K armed bandit class

---
+++ Multi-Armed Bandits/bandit.py
@@ -0,0 +1,22 @@
+from statistics import NormalDist
+
+class KArmedBandit:
+    """
+    Implements a K Armed Bandit
+
+    """
+    def __init__(self, arms=10, reward_mean=0, reward_std=1, sigma=1):
+        self.arms = arms
+        self.reward_mean = reward_mean
+        self.reward_std = reward_std
+        self.sigma = sigma
+
+        self.actual_reward_values = NormalDist(mu=reward_mean, sigma=reward_std).samples(arms)
+        self.reward_distributions = \
+            [NormalDist(mu=actual_reward_value, sigma=sigma) for actual_reward_value in self.actual_reward_values]
+        self.action_space = [x for x in range(self.arms)]
+    
+    def step(self, action):
+        assert action in self.action_space,  f"invalid action {action}, type: {type(action)}"
+        return self.reward_distributions[action].samples(1)
+
